ecccf53cb40a76f57d03900e4060749dc1801d8e
Esteban Puentes Roldan <rapuentes@uan.edu.co>
2022-06-14 11:22:17 -0500

    added routes to query by using aggregates querying, tour-stats and monthly-plan

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router();
 const tourController = require("../controllers/tourController")
-const {getAllTours,createTour,getTour,uptdateTour,deleteTour, checkID, checkBody, aliasTopTours} = tourController
+const {getAllTours,createTour,getTour,uptdateTour,deleteTour, checkID, checkBody, aliasTopTours, getTourStats, getMonthlyPlan} = tourController
 
 // router.param("id", checkID)
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours)
+router.route("/tour-stats").get(getTourStats)
+router.route("/monthly-plan/:year").get(getMonthlyPlan)
 router.route("/").get(getAllTours).post( createTour)
 router.route("/:id").get(getTour).patch(uptdateTour).delete(deleteTour)
 
